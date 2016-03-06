735701fccff1b8165ce5df275613a23e6b650ac7
captainwdy <captainwdy@gmail.com>
2016-03-06 13:37:11 +0800

    add sicp 1.2

---
+++ lisp/sicp/chap1.2.scm
@@ -0,0 +1,62 @@
+;;1.2.1
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+(factorial 100)
+
+(define (factorial n)
+  (define (fact-iter product counter)
+    (if (> counter n)
+        product
+        (fact-iter (* counter product)
+                   (+ counter 1))))
+  (fact-iter 1 1))
+(factorial 100)
+;;1.9
+递归
+迭代
+;;1.10 ackermann
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+(A 1 10)
+(A 2 4)
+(A 3 3)
+2n,2^n,2^2^2...^2,5n^2
+
+;;1.2.2
+(define (fib n)
+  (cond ((= n 0) 0)
+        ((= n 1) 1)
+        (else (+ (fib (- n 1))
+                 (fib (- n 2))))))
+(fib 10)
+
+(define (fib n)
+  (define (iter a b count)
+    (if (= count 0)
+        b
+        (iter (+ a b) a (- count 1))))
+  (iter 1 0 n))
+(fib 100)
+
+(define (count-change amount)
+  (define (cc amount kinds-of-coins)
+    (cond ((= amount 0) 1)
+          ((or (< amount 0) (= kinds-of-coins 0)) 0)
+          (else (+ (cc amount (- kinds-of-coins 1))
+                   (cc (- amount (first-denomination kinds-of-coins))
+                       kinds-of-coins)))))
+  (define (first-denomination kinds-of-coins)
+    (cond ((= kinds-of-coins 1) 1)
+          ((= kinds-of-coins 2) 5)
+          ((= kinds-of-coins 3) 10)
+          ((= kinds-of-coins 4) 25)
+          ((= kinds-of-coins 5) 50)))
+  (cc amount 5))
+(count-change 100)
+  
